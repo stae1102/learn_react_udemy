8e36ff21a1f2633b2035722c9539bcd82e82c078
Seongtae <stae1102@gmail.com>
2023-02-26 01:07:53 +0900

    fix: rename data handler to correct function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,7 +41,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = { title: enteredTitle, amount: enteredAmount, date: new Date(enteredDate) };
 
-    props.onSaveExpenseDataHandler(expenseData);
+    props.onSaveExpenseData(expenseData);
 
     setEnteredTitle('');
     setEnteredAmount('');
