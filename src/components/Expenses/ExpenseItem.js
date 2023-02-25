0be48a03fc63532d854a9d1c9c78dd431a8bf2a4
Seongtae <stae1102@gmail.com>
2023-02-26 01:08:41 +0900

    chore(ExpenseItem): delete useless function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import { useState } from 'react';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    const newTitle = 'Updated!';
-    setTitle(newTitle);
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
