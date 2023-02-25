248cc03984e3327ae658603ba3fda5df9aea6bbb
Seongtae <stae1102@gmail.com>
2023-02-26 01:08:55 +0900

    feat: Add filter function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,7 +7,6 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
-  console.log(filteredYear);
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -17,10 +16,19 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+        {props.items.map((expense) => {
+          if (expense.date.getFullYear().toString() === filteredYear) {
+            return (
+              <ExpenseItem
+                key={expense.id}
+                onChangeFilter={filterChangeHandler}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            );
+          }
+        })}
       </Card>
     </div>
   );
