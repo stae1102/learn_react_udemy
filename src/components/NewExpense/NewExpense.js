3dd6a904367edb118288ec8393b668fc870e381d
Seongtae <stae1102@gmail.com>
2023-02-25 23:32:39 +0900

    feat: lift state up

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,14 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
   const saveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+
+    props.onAddExpense(expenseData);
   };
 
   return (
