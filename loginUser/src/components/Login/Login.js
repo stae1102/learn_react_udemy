3d6742f1741b4d06a2e78e2aa8e874cf2fd25ba8
Seongtae <stae1102@gmail.com>
2023-03-01 15:30:37 +0900

    feat: Add a cleanup function

--- loginUser/src/components/Login/Login.js
+++ loginUser/src/components/Login/Login.js
@@ -12,7 +12,13 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    const identifier = setTimeout(() => {
+      setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
