8d5c40032840ab838ed5342ac84db58d319f9459
Seongtae <stae1102@gmail.com>
2023-03-03 20:46:12 +0900

    feat(AvailableMeals): Inject the MealItem component

--- ReactMeals/src/components/Meals/AvailableMeals.js
+++ ReactMeals/src/components/Meals/AvailableMeals.js
@@ -3,9 +3,12 @@ import { DUMMY_MEALS } from './dummy-meals';
 import Card from '../UI/Card';
 
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+  ));
 
   return (
     <section className={classes.meals}>
