2eeea4793bc622f7af14051b9982fefb173c6734
Seongtae <stae1102@gmail.com>
2023-03-03 23:52:25 +0900

    feat: Activate modal controlling

--- ReactMeals/src/App.js
+++ ReactMeals/src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
