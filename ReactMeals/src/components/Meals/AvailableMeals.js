6cf9dd1dec9e1b0cb657a15f3fb6e69539b374ed
Seongtae <stae1102@gmail.com>
2023-03-03 20:51:22 +0900

    fix: change duplicated id value of the meal

--- ReactMeals/src/components/Meals/AvailableMeals.js
+++ ReactMeals/src/components/Meals/AvailableMeals.js
@@ -7,7 +7,7 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+    <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
   ));
 
   return (
