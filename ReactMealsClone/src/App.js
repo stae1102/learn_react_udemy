417cf3471b2a98ba9cdf99db3633d07a06c553dc
Seongtae <stae1102@gmail.com>
2023-03-05 15:54:15 +0900

    feat: Manage a state of cart visibility

--- ReactMealsClone/src/App.js
+++ ReactMealsClone/src/App.js
@@ -1,14 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Cart from './components/Cart/Cart';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const closeCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onCloseCart={closeCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
