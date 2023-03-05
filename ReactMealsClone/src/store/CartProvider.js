3c866249836b64e94140926681a89b6b9dcfb687
Seongtae <stae1102@gmail.com>
2023-03-05 18:16:25 +0900

    feat: Remove items from the cart

--- ReactMealsClone/src/store/CartProvider.js
+++ ReactMealsClone/src/store/CartProvider.js
@@ -3,7 +3,7 @@ import CartContext from './cart-context';
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const totalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
     const existingCartItemIndex = state.items.findIndex((item) => {
       return item.id === action.item.id;
@@ -22,7 +22,27 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
-    return { items: updatedItems, totalAmount };
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
 
   return defaultCartState;
