6f85461e9ae8659543f0fd111e51a8ab4e787b2c
Seongtae <stae1102@gmail.com>
2023-03-05 14:10:52 +0900

    feat: Create a MealItemForm component

--- ReactMealsClone/src/components/Meals/MealItem/MealItemForm.js
+++ ReactMealsClone/src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,20 @@
+import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <label htmlFor='amount' className={classes}>
-        Amount
-      </label>
-      <input />
+      <Input
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
       <button type='submit'>+ Add</button>
     </form>
   );
