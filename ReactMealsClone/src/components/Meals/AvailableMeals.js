4524188431b7bbdea505ff39d55b1cf09d7dcfda
Seongtae <stae1102@gmail.com>
2023-03-05 13:42:39 +0900

    feat: Apply Card and MealItem component

--- ReactMealsClone/src/components/Meals/AvailableMeals.js
+++ ReactMealsClone/src/components/Meals/AvailableMeals.js
@@ -1,12 +1,20 @@
 import { DUMMY_MEALS } from './dummy-meals';
+
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
+
 import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
