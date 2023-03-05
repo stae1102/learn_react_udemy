f4c1aa8b78bbc922c97179d426ae2933206fba11
Seongtae <stae1102@gmail.com>
2023-03-05 14:11:43 +0900

    fix: Give a id value to MealItemForm

--- ReactMealsClone/src/components/Meals/MealItem/MealItem.js
+++ ReactMealsClone/src/components/Meals/MealItem/MealItem.js
@@ -9,7 +9,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{props.price}</div>
       </div>
-      <MealItemForm />
+      <MealItemForm id={props.id} />
     </li>
   );
 };
