9d5612c64967d9f24bc6dfae7c2fa7f9ce665fdc
Seongtae <stae1102@gmail.com>
2023-03-05 17:58:54 +0900

    feat: Check existing items and Increment amount

--- ReactMealsClone/src/store/CartProvider.js
+++ ReactMealsClone/src/store/CartProvider.js
@@ -3,8 +3,24 @@ import CartContext from './cart-context';
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const totalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.item.id;
+    });
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
 
     return { items: updatedItems, totalAmount };
   }
