d98e5c0f2108c8e5ed1b9a89c85326015e03b915
Seongtae <stae1102@gmail.com>
2023-03-05 15:42:29 +0900

    fix: Correct a typo in the Modal component

--- ReactMealsClone/src/components/UI/Modal.js
+++ ReactMealsClone/src/components/UI/Modal.js
@@ -9,7 +9,7 @@ const Backdrop = (props) => {
 
 const ModalOverlay = (props) => {
   return (
-    <div className={classes.model}>
+    <div className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
     </div>
   );
