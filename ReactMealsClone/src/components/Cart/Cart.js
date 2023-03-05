60fbd668e0b3b39e4a5c1105671eee40cb9b6c84
Seongtae <stae1102@gmail.com>
2023-03-05 15:49:04 +0900

    feat: Create a draft Cart component

---
+++ ReactMealsClone/src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 1, name: 'Sushi', amount: 1, price: 15.99 }].map((item) => (
+        <CartItem key={item.id} {...item} />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
