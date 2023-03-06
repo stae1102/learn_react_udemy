34efade92cb6d74b48e00d97d6a85f93d6c8b3b9
Seongtae <stae1102@gmail.com>
2023-03-07 07:24:06 +0900

    replace: relocate code

--- NewProject/src/App.js
+++ NewProject/src/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-  return <div className='app'></div>;
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
+  return (
+    <div className='app'>
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
+  );
 }
 
 export default App;
