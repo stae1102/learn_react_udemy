555a9425cadab4592dbd0ee27d6f997114e7228f
Seongtae <stae1102@gmail.com>
2023-03-07 18:19:56 +0900

    feat: Wrap the DemoList with react memo

--- NewProjectUseMemo/src/components/Demo/DemoList.js
+++ NewProjectUseMemo/src/components/Demo/DemoList.js
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import classes from './DemoList.module.css';
 
 const DemoList = (props) => {
   const sortedList = props.items.sort((a, b) => a - b);
+  console.log('DemoList RUNNING');
 
   return (
     <div className={classes.list}>
@@ -15,4 +18,4 @@ const DemoList = (props) => {
   );
 };
 
-export default DemoList;
+export default React.memo(DemoList);
