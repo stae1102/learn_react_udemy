935a7e2a121cd52f48c142762c62d3dd053008fe
Seongtae <stae1102@gmail.com>
2023-03-13 18:10:38 +0900

    feat: Revert the POST request

--- SimpleTaskApplication/src/components/NewTask/NewTask.js
+++ SimpleTaskApplication/src/components/NewTask/NewTask.js
@@ -2,40 +2,38 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
-import useManageTask from '../../hooks/use-manage-task';
 
 const NewTask = (props) => {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  // const enterTaskHandler = async (taskText) => {
-  //   setIsLoading(true);
-  //   setError(null);
-  //   try {
-  //     const response = await fetch('https://react-http-10279-default-rtdb.firebaseio.com/tasks.json', {
-  //       method: 'POST',
-  //       body: JSON.stringify({ text: taskText }),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //     });
+  const enterTaskHandler = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://react-http-10279-default-rtdb.firebaseio.com/tasks.json', {
+        method: 'POST',
+        body: JSON.stringify({ text: taskText }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  //     if (!response.ok) {
-  //       throw new Error('Request failed!');
-  //     }
+      if (!response.ok) {
+        throw new Error('Request failed!');
+      }
 
-  //     const data = await response.json();
+      const data = await response.json();
 
-  //     const generatedId = data.name; // firebase-specific => "name" contains generated id
-  //     const createdTask = { id: generatedId, text: taskText };
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
 
-  //     props.onAddTask(createdTask);
-  //   } catch (err) {
-  //     setError(err.message || 'Something went wrong!');
-  //   }
-  //   setIsLoading(false);
-  // };
-  const [isLoading, error, enterTaskHandler] = useManageTask('POST', props.onAddTask);
+      props.onAddTask(createdTask);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  };
 
   return (
     <Section>
