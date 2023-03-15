2acd49bb632198e8b6d35fca3e4c949c39cef0a0
Seongtae <stae1102@gmail.com>
2023-03-15 23:42:43 +0900

    feat: Create an use-input hook

---
+++ AdvancedForm/src/hooks/use-input.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const Input = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  return { value: enteredValue, hasError, valueIsValid, valueChangeHandler, inputBlurHandler };
+};
+
+export default Input;
