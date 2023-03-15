ec63403c492c8e2dca269a5492d034005ad00da1
Seongtae <stae1102@gmail.com>
2023-03-15 21:29:06 +0900

    feat: Warn user when the focus is missing

--- AdvancedForm/src/components/SimpleInput.js
+++ AdvancedForm/src/components/SimpleInput.js
@@ -16,6 +16,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -44,7 +53,14 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
         {nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className='form-actions'>
