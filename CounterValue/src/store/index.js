116e84d7ac2e2156133d3ff70a20ee08973851c8
Seongtae <stae1102@gmail.com>
2023-03-20 23:40:18 +0900

    feat: Create a redux store

---
+++ CounterValue/src/store/index.js
@@ -0,0 +1,23 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'INCREMENT') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'DECREMENT') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = configureStore({
+  reducer: counterReducer,
+});
+
+export default store;
