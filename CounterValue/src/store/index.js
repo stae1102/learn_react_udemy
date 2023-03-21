c857a1e094902d447d84a65bffd36032691b641b
Seongtae <stae1102@gmail.com>
2023-03-21 18:27:48 +0900

    feat: Toggle counter state

--- CounterValue/src/store/index.js
+++ CounterValue/src/store/index.js
@@ -1,21 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'INCREMENT') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'INCREASE') {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === 'DECREMENT') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === 'TOGGLE') {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 
