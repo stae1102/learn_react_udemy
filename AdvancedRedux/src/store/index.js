e8de3a29f255290167ffe137362a53f7544317b2
Seongtae <stae1102@gmail.com>
2023-03-22 19:10:34 +0900

    feat: Toggle cart component

--- AdvancedRedux/src/store/index.js
+++ AdvancedRedux/src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiSlice from './ui-slice';
 
-const store = configureStore({ reducer: uiSlice.reducer });
+const store = configureStore({ reducer: { ui: uiSlice.reducer } });
 
 export default store;
