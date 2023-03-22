cbd7745d8dfcd380c5a9a97b85bf57b986db60a2
Seongtae <stae1102@gmail.com>
2023-03-22 19:26:38 +0900

    feat: Deliver mycart slice action

--- AdvancedRedux/src/components/Shop/ProductItem.js
+++ AdvancedRedux/src/components/Shop/ProductItem.js
@@ -1,8 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { myCartActions } from '../../store/mycart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    dispatch(
+      myCartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +27,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
