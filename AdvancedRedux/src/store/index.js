c7045da384c0fff1f0d4c79955c4603cdae283a9
Seongtae <stae1102@gmail.com>
2023-03-22 19:22:10 +0900

    feat: Create a mycart slice

--- AdvancedRedux/src/store/index.js
+++ AdvancedRedux/src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import myCartSlice from './mycart-slice';
 import uiSlice from './ui-slice';
 
-const store = configureStore({ reducer: { ui: uiSlice.reducer } });
+const store = configureStore({ reducer: { ui: uiSlice.reducer, myCart: myCartSlice.reducer } });
 
 export default store;
