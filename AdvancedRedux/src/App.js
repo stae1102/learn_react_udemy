a9c06f9ce4759d12109cd0fbb81e83f870059cc7
Seongtae <stae1102@gmail.com>
2023-03-23 23:30:59 +0900

    feat: Add notification to ui slice

--- AdvancedRedux/src/App.js
+++ AdvancedRedux/src/App.js
@@ -1,11 +1,63 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 import { useSelector } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 
 function App() {
+  const dispatch = useDispatch();
+
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const myCart = useSelector((state) => state.myCart);
+
+  useEffect(() => {
+    if (myCart.items.length === 0) {
+      return;
+    }
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = fetch(
+        'https://react-http-10279-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(myCart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    });
+  }, [myCart, dispatch]);
 
   return (
     <Layout>
