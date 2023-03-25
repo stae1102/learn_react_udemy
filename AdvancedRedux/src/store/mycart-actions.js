370f8f3441dbcc0a009410fc41e0daf3c00f5cba
Seongtae <stae1102@gmail.com>
2023-03-25 09:19:43 +0900

    feat: Get cart data from the firebase

--- AdvancedRedux/src/store/mycart-actions.js
+++ AdvancedRedux/src/store/mycart-actions.js
@@ -1,4 +1,34 @@
 import { uiActions } from './ui-slice';
+import { myCartActions } from './mycart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const resposne = await fetch(
+        'https://react-http-10279-default-rtdb.firebaseio.com/cart.json'
+      );
+
+      if (!resposne.ok) {
+        throw new Error('Could not fetch cart data');
+      }
+
+      const data = await resposne.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(myCartActions.replaceCart(cartData));
+    } catch (error) {
+      uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!',
+      });
+    }
+  };
+};
 
 export const sendCartData = (myCart) => {
   return async (dispatch) => {
