ca53831cbf85ea923299d766035060fa37a8056c
Seongtae <stae1102@gmail.com>
2023-03-27 06:50:01 +0900

    feat: Create a product page

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -1,8 +1,12 @@
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Homepage from './pages/Home';
+import ProductsPage from './pages/Products';
 
-const router = createBrowserRouter([{ path: '/', element: <Homepage /> }]);
+const router = createBrowserRouter([
+  { path: '/', element: <Homepage /> },
+  { path: '/products', element: <ProductsPage /> },
+]);
 
 const App = () => {
   return <RouterProvider router={router} />;
