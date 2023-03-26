926c444e4563d4886e559dd707e82cacda4490f3
Seongtae <stae1102@gmail.com>
2023-03-26 18:12:20 +0900

    feat: Enroll homepage component on root path

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -1,5 +1,11 @@
-function App() {
-  return <div></div>;
-}
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+
+import Homepage from './pages/Home';
+
+const router = createBrowserRouter([{ path: '/', element: <Homepage /> }]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
