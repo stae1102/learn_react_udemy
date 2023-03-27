1515c08f846bd1782eeceece7f71e68a213b296e
Seongtae <stae1102@gmail.com>
2023-03-27 18:34:12 +0900

    feat: Apply an error element to app component

--- ReactRouter/src/App.js
+++ ReactRouter/src/App.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/Error';
 
 import Homepage from './pages/Home';
 import ProductsPage from './pages/Products';
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <Homepage /> },
       { path: '/products', element: <ProductsPage /> },
