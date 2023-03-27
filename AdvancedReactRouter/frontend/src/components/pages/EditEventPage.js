df470ee25e3007055a9875317a1b84686132e3b0
Seongtae <stae1102@gmail.com>
2023-03-28 07:09:30 +0900

    feat: Add all needed pages

---
+++ AdvancedReactRouter/frontend/src/components/pages/EditEventPage.js
@@ -0,0 +1,5 @@
+const EditEventPage = () => {
+  return <h1>My Edit Event Page!</h1>;
+};
+
+export default EditEventPage;
---
+++ AdvancedReactRouter/frontend/src/components/pages/NewEventPage.js
@@ -0,0 +1,5 @@
+const NewEventPage = () => {
+  return <h1>My New Event Page!</h1>;
+};
+
+export default NewEventPage;
