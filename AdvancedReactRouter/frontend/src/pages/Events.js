5fa0ddd2e237213470f1f8a0d8a8f42642d138cf
Seongtae <stae1102@gmail.com>
2023-03-29 07:01:04 +0900

    refactor: Move loader app to events page

--- AdvancedReactRouter/frontend/src/pages/Events.js
+++ AdvancedReactRouter/frontend/src/pages/Events.js
@@ -2,10 +2,21 @@ import { useLoaderData } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
-function EventsPage() {
+const EventsPage = () => {
   const events = useLoaderData();
 
   return <EventsList events={events} />;
-}
+};
 
 export default EventsPage;
+
+export const loader = async () => {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+};
