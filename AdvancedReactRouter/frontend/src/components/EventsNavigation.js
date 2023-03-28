09a10892e993a35ff8e78e058c11173cdc9c91a1
Seongtae <stae1102@gmail.com>
2023-03-28 19:35:16 +0900

    feat: Replace anchor tag with NavLink

--- AdvancedReactRouter/frontend/src/components/EventsNavigation.js
+++ AdvancedReactRouter/frontend/src/components/EventsNavigation.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import classes from './EventsNavigation.module.css';
 
 function EventsNavigation() {
@@ -6,10 +7,25 @@ function EventsNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <a href="/events">All Events</a>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              All Events
+            </NavLink>
           </li>
           <li>
-            <a href="/events/new">New Event</a>
+            <NavLink
+              to='new'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              New Event
+            </NavLink>
           </li>
         </ul>
       </nav>
--- AdvancedReactRouter/frontend/src/pages/EventDetail.js
+++ AdvancedReactRouter/frontend/src/pages/EventDetail.js
@@ -3,7 +3,12 @@ import { useParams } from 'react-router-dom';
 const EventDetailPage = () => {
   const params = useParams();
 
-  return <h1>My Event {params.eventId} Page!</h1>;
+  return (
+    <>
+      <h1>My Event Detail Page!</h1>
+      <p>Event ID: {params.eventId}</p>
+    </>
+  );
 };
 
 export default EventDetailPage;
