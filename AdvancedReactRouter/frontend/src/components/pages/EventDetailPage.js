1e5da099b41ee26e0ef5c5e0c8253e1bbb2a6529
Seongtae <stae1102@gmail.com>
2023-03-28 19:04:37 +0900

    feat: Show event number dynamically

--- AdvancedReactRouter/frontend/src/components/pages/EventDetailPage.js
+++ AdvancedReactRouter/frontend/src/components/pages/EventDetailPage.js
@@ -1,5 +1,9 @@
+import { useParams } from 'react-router-dom';
+
 const EventDetailPage = () => {
-  return <h1>My Event Detail Page!</h1>;
+  const params = useParams();
+
+  return <h1>My Event {params.eventId} Page!</h1>;
 };
 
 export default EventDetailPage;
