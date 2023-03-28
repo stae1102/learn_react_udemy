b8c92f74e3d2d4eaa19e390e6a88c7e1609493bb
Seongtae <stae1102@gmail.com>
2023-03-28 18:59:22 +0900

    feat: Move to event detail page by using route

--- AdvancedReactRouter/frontend/src/components/pages/EventsPage.js
+++ AdvancedReactRouter/frontend/src/components/pages/EventsPage.js
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+
 const EventsPage = () => {
-  return <h1>My Events Page!</h1>;
+  return (
+    <>
+      <h1>My Events Page!</h1>
+      <ul>
+        <li>
+          <Link to='1' relative='route'>
+            Event 1
+          </Link>
+        </li>
+        <li>
+          <Link to='2' relative='route'>
+            Event 2
+          </Link>
+        </li>
+        <li>
+          <Link to='3' relative='route'>
+            Event 3
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default EventsPage;
