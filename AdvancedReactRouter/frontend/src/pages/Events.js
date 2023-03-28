ca0ee8401bacaa20a99be546204fb5479758aae0
Seongtae <stae1102@gmail.com>
2023-03-29 06:45:52 +0900

    refactor: Get data by fetching backend

--- AdvancedReactRouter/frontend/src/pages/Events.js
+++ AdvancedReactRouter/frontend/src/pages/Events.js
@@ -1,31 +1,37 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-const DUMMY_EVENTS = [
-  {
-    id: 'e1',
-    title: 'I Love Kimchi',
-  },
-  {
-    id: 'e2',
-    title: `What doesn't kill you make you stronger`,
-  },
-];
+import EventsList from '../components/EventsList';
 
-const EventsPage = () => {
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/events');
+
+      if (!response.ok) {
+        setError('Fetching events failed.');
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>My Events Page!</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id} relative='route'>
-              {event.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
-};
+}
 
 export default EventsPage;
