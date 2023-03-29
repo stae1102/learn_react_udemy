90ffbd948573c4cb2ae787055a5a776092c04e3a
Seongtae <stae1102@gmail.com>
2023-03-29 20:40:39 +0900

    feat: Get event detail data by using loader

--- AdvancedReactRouter/frontend/src/App.js
+++ AdvancedReactRouter/frontend/src/App.js
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Homepage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage from './pages/EventDetail';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './components/layout/Root';
@@ -24,7 +26,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ':eventId', element: <EventDetailPage /> },
+          {
+            path: ':eventId',
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
         ],
--- AdvancedReactRouter/frontend/src/pages/EventDetail.js
+++ AdvancedReactRouter/frontend/src/pages/EventDetail.js
@@ -12,3 +12,20 @@ const EventDetailPage = () => {
 };
 
 export default EventDetailPage;
+
+export const loader = async ({ request, params }) => {
+  const id = params.eventId;
+
+  const response = await fetch(`http://localhost:8080/events/${id}`);
+
+  if (!response.ok) {
+    throw json(
+      { message: 'Could not fetch details for selected event.' },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return response;
+};
