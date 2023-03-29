334ee44ef5d02251f0195c10c82820332814ef96
Seongtae <stae1102@gmail.com>
2023-03-29 19:14:37 +0900

    feat: Handle not found error

--- AdvancedReactRouter/frontend/src/pages/Events.js
+++ AdvancedReactRouter/frontend/src/pages/Events.js
@@ -6,16 +6,20 @@ const EventsPage = () => {
   const data = useLoaderData();
   const events = data.events;
 
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
   return <EventsList events={events} />;
 };
 
 export default EventsPage;
 
 export const loader = async () => {
-  const response = await fetch('http://localhost:8080/events');
+  const response = await fetch('http://localhost:8080/evaents');
 
   if (!response.ok) {
-    // ...
+    throw { message: 'Could not fetch events.' };
   } else {
     return response;
   }
