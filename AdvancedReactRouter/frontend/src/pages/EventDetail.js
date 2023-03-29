21e285adbff6c4e22cf7156f08203925af9cd626
Seongtae <stae1102@gmail.com>
2023-03-29 20:56:53 +0900

    feat: Use useRouterLoaderData hook

--- AdvancedReactRouter/frontend/src/pages/EventDetail.js
+++ AdvancedReactRouter/frontend/src/pages/EventDetail.js
@@ -1,9 +1,9 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { json, useRouteLoaderData } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
 const EventDetailPage = () => {
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
 
   return <EventItem event={data.event} />;
 };
