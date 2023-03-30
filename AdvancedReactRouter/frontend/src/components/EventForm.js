b4c9b2055992e73b303746550c6e1050e4186ae1
Seongtae <stae1102@gmail.com>
2023-03-30 18:53:43 +0900

    refactor: Move form action to event form component

--- AdvancedReactRouter/frontend/src/components/EventForm.js
+++ AdvancedReactRouter/frontend/src/components/EventForm.js
@@ -80,3 +80,38 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export const action = async ({ request, params }) => {
+  const method = request.method;
+  const data = await request.formData();
+
+  const enteredData = {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let url = 'http://localhost:8080/events';
+
+  if (method === 'PATCH') {
+    const eventId = params.eventId;
+    url += `/${eventId}`;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(enteredData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: 'Could not save event.' }, { status: 500 });
+  }
+
+  return redirect('/events');
+};
