401b2ad0a139332eab2c2d93e1055e629b50ed58
Seongtae <stae1102@gmail.com>
2023-04-05 07:24:22 +0900

    feat: Show meetups at HomePage

---
+++ NextJSBlog/pages/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://ntvb.tmsimg.com/assets/p12386480_v_h10_az.jpg?w=960&h=540',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://ntvb.tmsimg.com/assets/p12386480_v_h10_az.jpg?w=960&h=540',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
