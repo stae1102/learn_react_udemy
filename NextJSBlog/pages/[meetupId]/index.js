eac9ab9ee8e5636fbcd326f090f0fe6866242e89
Seongtae <stae1102@gmail.com>
2023-04-06 07:16:12 +0900

    feat: Get meetup detail data with hard coded info

--- NextJSBlog/pages/[meetupId]/index.js
+++ NextJSBlog/pages/[meetupId]/index.js
@@ -11,4 +11,39 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://ntvb.tmsimg.com/assets/p12386480_v_h10_az.jpg?w=960&h=540',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
