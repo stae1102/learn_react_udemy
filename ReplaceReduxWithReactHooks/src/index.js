bd79c13309a8adcead293fb8b6dc73a9d8701114
Seongtae <stae1102@gmail.com>
2023-04-11 22:24:50 +0900

    feat: Replace Redux provider with Context

--- ReplaceReduxWithReactHooks/src/index.js
+++ ReplaceReduxWithReactHooks/src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers } from 'redux';
 
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context';
 
 const rootReducer = combineReducers({
   shop: productReducer,
 });
 
-const store = createStore(rootReducer);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <App />
-  </Provider>
+  </ProductsProvider>
 );
