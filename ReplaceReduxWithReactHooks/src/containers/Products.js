c91e69586336bd4c0f4afc9b7085e914369caddf
Seongtae <stae1102@gmail.com>
2023-04-11 23:00:05 +0900

    feat: Get products by using useContext

--- ReplaceReduxWithReactHooks/src/containers/Products.js
+++ ReplaceReduxWithReactHooks/src/containers/Products.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useContext } from 'react';
 
 import ProductItem from '../components/Products/ProductItem';
+import { ProductsContext } from '../context/products-context';
 import './Products.css';
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
   return (
-    <ul className="products-list">
-      {productList.map(prod => (
+    <ul className='products-list'>
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
           title={prod.title}
           description={prod.description}
           isFav={prod.isFavorite}
+          // {...prod}
         />
       ))}
     </ul>
