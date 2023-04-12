554b30d96c435d61cdb394c20f7b42340df94e54
Seongtae <stae1102@gmail.com>
2023-04-13 07:09:10 +0900

    test: Make a testing group

--- ReactTest/src/components/Greeting.test.js
+++ ReactTest/src/components/Greeting.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders greeting link', () => {
-  render(<Greeting />);
-  const helloWorldElement = screen.getByText('Hello world', { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+describe('Greeting component', () => {
+  test('renders greeting link', () => {
+    render(<Greeting />);
+    const helloWorldElement = screen.getByText('Hello world', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
