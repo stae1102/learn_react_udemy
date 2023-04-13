d46dbc73317ed0cfbfb9cd9e5fec872414b5ac65
Seongtae <stae1102@gmail.com>
2023-04-13 18:30:30 +0900

    test: Check if there are text

--- ReactTest/src/components/Greeting.test.js
+++ ReactTest/src/components/Greeting.test.js
@@ -7,4 +7,13 @@ describe('Greeting component', () => {
     const helloWorldElement = screen.getByText('Hello world', { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
   });
+
+  test('renders good to see you if the button was NOT clicked', () => {
+    render(<Greeting />);
+    const goodByElement = screen.getByText(`It's good to see you!`, {
+      exact: false,
+    });
+    expect(goodByElement).toBeInTheDocument();
+  });
+
 });
