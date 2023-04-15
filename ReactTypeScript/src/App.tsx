4b4d089b386984ca08b1e9f7a67102d5059676ec
Seongtae <stae1102@gmail.com>
2023-04-15 09:29:59 +0900

    feat: Transfer todos using data model

--- ReactTypeScript/src/App.tsx
+++ ReactTypeScript/src/App.tsx
@@ -1,9 +1,12 @@
 import Todos from './components/Todo';
+import Todo from './components/models/todo';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
   return (
     <div>
-      <Todos items={['Learn React', 'Learn TypeScript']} />
+      <Todos items={todos} />
     </div>
   );
 }
