b3eefeb173a1486879688a59e30745cbaab5b95f
Seongtae <stae1102@gmail.com>
2023-04-17 23:06:18 +0900

    feat: Add and delete ingredients

--- ReactHooks/src/components/Ingredients/IngredientForm.js
+++ ReactHooks/src/components/Ingredients/IngredientForm.js
@@ -13,7 +13,7 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    const updatedIngredients = userIngredients.filter(
+      (ingredient) => ingredient.id !== ingredientId
+    );
+    setUserIngredients(updatedIngredients);
+  };
+
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
