dcbc24833904166b44ae099b4f8143885140a5e9
Seongtae <stae1102@gmail.com>
2023-04-19 07:14:50 +0900

    style: Delete useless annotation

--- ReactHooks/src/components/Ingredients/IngredientForm.js
+++ ReactHooks/src/components/Ingredients/IngredientForm.js
@@ -7,10 +7,6 @@ const IngredientForm = React.memo((props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
 
-  // const nameChangeHandler = event => {
-  //   setName(event.target.value);
-  // }
-
   const submitHandler = (event) => {
     event.preventDefault();
     props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
