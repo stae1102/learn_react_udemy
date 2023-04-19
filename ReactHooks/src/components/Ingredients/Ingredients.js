f440028c3277fbe50f727bb2054250d40baeca86
Seongtae <stae1102@gmail.com>
2023-04-20 00:58:49 +0900

    feat: Process http error

--- ReactHooks/src/components/Ingredients/Ingredients.js
+++ ReactHooks/src/components/Ingredients/Ingredients.js
@@ -2,11 +2,13 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const loadedIngredients = [];
@@ -37,20 +39,24 @@ const Ingredients = () => {
 
   const addIngredientHandler = async (ingredient) => {
     setIsLoading(true);
-    const response = await fetch(
-      'https://react-http-10279-default-rtdb.firebaseio.com/ingredients.json',
-      {
-        method: 'POST',
-        body: JSON.stringify(ingredient),
-        headers: { 'Content-Type': 'application/json' },
-      }
+    try {
+      const response = await fetch(
+        'https://react-http-10279-default-rtdb.firebaseio.com/ingredients.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(ingredient),
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
       setIsLoading(false);
-    );
-    const responseData = await response.json();
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: responseData.name, ...ingredient },
-    ]);
+      const responseData = await response.json();
+      setUserIngredients((prevIngredients) => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient },
+      ]);
+    } catch (error) {
+      setError('Something went wrong!');
+    }
   };
 
   const removeIngredientHandler = async (ingredientId) => {
@@ -60,17 +66,26 @@ const Ingredients = () => {
       {
         method: 'DELETE',
       }
-    setIsLoading(false);
     );
+    setIsLoading(false);
     const updatedIngredients = userIngredients.filter(
       (ingredient) => ingredient.id !== ingredientId
     );
     setUserIngredients(updatedIngredients);
   };
 
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  };
+
   return (
     <div className='App'>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
         loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
