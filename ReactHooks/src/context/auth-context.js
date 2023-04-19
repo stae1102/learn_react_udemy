083a685120984fb49d38f783a2fe8d8ab7678639
Seongtae <stae1102@gmail.com>
2023-04-20 07:12:06 +0900

    feat: Create auth context

---
+++ ReactHooks/src/context/auth-context.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export const AuthContext = React.createContext({
+  isAuth: false,
+  login: () => {},
+});
+
+const AuthContextProvider = (props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginHandelr = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ login: loginHandelr, isAuth: isAuthenticated }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
