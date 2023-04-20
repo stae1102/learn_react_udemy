2c78031bec0adbaf0988ccf34bf83dff0820c99a
Seongtae <stae1102@gmail.com>
2023-04-21 07:25:20 +0900

    feat: Implement useHttp hook

--- ReactHooks/src/hooks/use-http.js
+++ ReactHooks/src/hooks/use-http.js
@@ -3,9 +3,20 @@ import { useReducer, useCallback } from 'react';
 const httpReducer = (httpState, action) => {
   switch (action.type) {
     case 'SEND':
-      return { loading: true, error: null, data: null };
+      return {
+        loading: true,
+        error: null,
+        data: null,
+        extra: null,
+        identifier: action.identifier,
+      };
     case 'RESPONSE':
-      return { ...httpState, loading: false, data: action.responseData };
+      return {
+        ...httpState,
+        loading: false,
+        data: action.responseData,
+        extra: action.extra,
+      };
     case 'ERROR':
       return { loading: false, error: action.errorMessage };
     case 'CLEAR':
@@ -15,39 +26,46 @@ const httpReducer = (httpState, action) => {
   }
 };
 
-const useHttp = (url, method, body) => {
+const useHttp = () => {
   const [httpState, dispatchHttpState] = useReducer(httpReducer, {
     loading: false,
     error: null,
     data: null,
+    extra: null,
+    identifier: null,
   });
 
-  const sendRequest = useCallback(async () => {
-    dispatchHttpState({ type: 'SEND' });
+  const sendRequest = useCallback(
+    async (url, method, body, reqExtra, reqIdentifier) => {
+      dispatchHttpState({ type: 'SEND', identifier: reqIdentifier });
 
-    try {
-      const response = await fetch(url, {
-        method,
-        body,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      const responseData = await response.json();
-      dispatchHttpState({ type: 'RESPONSE', responseData });
-    } catch (error) {
-      dispatchHttpState({
-        type: 'ERROR',
-        errorMessage: 'Something went wrong!',
-      });
-    }
-  }, [url, method, body]);
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        const responseData = await response.json();
+        dispatchHttpState({ type: 'RESPONSE', responseData, extra: reqExtra });
+      } catch (error) {
+        dispatchHttpState({
+          type: 'ERROR',
+          errorMessage: 'Something went wrong!',
+        });
+      }
+    },
+    []
+  );
 
   return {
     isLoading: httpState.isLoading,
     data: httpState.data,
     error: httpState.error,
     sendRequest,
+    requestExtra: httpState.extra,
+    reqIdentifier: httpState.identifier,
   };
 };
 
