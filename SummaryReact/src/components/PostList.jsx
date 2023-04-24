f56ce6382e3b07a8f5152358aeeb7f86ec79b777
Seongtae <stae1102@gmail.com>
2023-04-24 21:46:35 +0900

    feat: Wrap new posts with modal

--- SummaryReact/src/components/PostList.jsx
+++ SummaryReact/src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
+import Modal from './Modal';
 import classes from './PostList.module.css';
 
 const PostList = (props) => {
@@ -18,10 +19,12 @@ const PostList = (props) => {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Manuel' body='Check out the full course!' />
