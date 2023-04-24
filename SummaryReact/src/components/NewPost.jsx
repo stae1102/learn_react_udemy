3990a34a57c104510a018ee354063679a2e34be9
Seongtae <stae1102@gmail.com>
2023-04-24 21:40:05 +0900

    feat: Handle entered body by parent props

--- SummaryReact/src/components/NewPost.jsx
+++ SummaryReact/src/components/NewPost.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
-
 import classes from './NewPost.module.css';
 
-function NewPost() {
-  const [enteredBody, setEnteredBody] = useState('');
-
-  const changeBodyHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor='body'>Text</label>
-        <textarea id='body' required rows={3} onChange={changeBodyHandler} />
+        <textarea id='body' required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
+      <p>{props.enteredBody}</p>
       <p>
         <label htmlFor='name'>Your name</label>
-        <input type='text' id='name' required />
+        <input type='text' id='name' required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
