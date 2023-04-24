b6ba37e21e8aa720bae8c26e93e995c588264ccd
Seongtae <stae1102@gmail.com>
2023-04-24 21:38:54 +0900

    feat: Move posts to PostList component

--- SummaryReact/src/App.jsx
+++ SummaryReact/src/App.jsx
@@ -1,15 +1,9 @@
-import Post from './components/Post';
 import PostList from './components/PostList';
 
 function App() {
-  const posts = [
-    { author: 'Maximilian', body: 'React.js is awesome!' },
-    { author: 'Manuel', body: 'Check out the full course!' },
-  ];
-
   return (
     <main>
-      <PostList posts={posts} />
+      <PostList />
     </main>
   );
 }
