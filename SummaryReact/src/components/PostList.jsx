35f94ef46afb702a5ecc73cf1212c3c7780af84e
Seongtae <stae1102@gmail.com>
2023-04-24 21:51:04 +0900

    feat: Show modal conditionally

--- SummaryReact/src/components/PostList.jsx
+++ SummaryReact/src/components/PostList.jsx
@@ -6,8 +6,13 @@ import Modal from './Modal';
 import classes from './PostList.module.css';
 
 const PostList = (props) => {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
 
   const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value);
@@ -19,12 +24,14 @@ const PostList = (props) => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={bodyChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author='Manuel' body='Check out the full course!' />
