896eef3b004d42ce07058ef7ae40fa3bcf19c084
Seongtae <stae1102@gmail.com>
2023-04-27 06:57:44 +0900

    feat: Move states to NewPost

--- SummaryReact/src/components/NewPost.jsx
+++ SummaryReact/src/components/NewPost.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost(props) {
+function NewPost({ onCancel }) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor='body'>Text</label>
-        <textarea id='body' required rows={3} onChange={props.onBodyChange} />
+        <textarea id='body' required rows={3} onChange={bodyChangeHandler} />
       </p>
-      <p>{props.enteredBody}</p>
       <p>
         <label htmlFor='name'>Your name</label>
         <input type='text' id='name' required onChange={authorChangeHandler} />
