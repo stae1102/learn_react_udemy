e477be859ddc8b40d3ee48d6ee812c3e941074f8
Seongtae <stae1102@gmail.com>
2023-04-29 10:47:56 +0900

    feat: Show posts

--- SummaryReact/src/components/PostList.jsx
+++ SummaryReact/src/components/PostList.jsx
@@ -12,6 +12,10 @@ const PostList = ({ isPosting, onStopPosting }) => {
     setPosts((prevPosts) => [postData, ...prevPosts]);
   };
 
+  const postContents = posts.map((post) => (
+    <Post key={post.body} author={post.author} body={post.body} />
+  ));
+
   return (
     <>
       {isPosting && (
@@ -19,9 +23,13 @@ const PostList = ({ isPosting, onStopPosting }) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author='Manuel' body='Check out the full course!' />
-      </ul>
+      {posts.length > 0 && <ul className={classes.posts}>{postContents}</ul>}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 };
