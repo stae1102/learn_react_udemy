1e1424974d01704c7d74d352824bfab59a413f72
Seongtae <stae1102@gmail.com>
2023-04-30 20:24:36 +0900

    feat: Show loading posts

--- SummaryReact/src/components/PostList.jsx
+++ SummaryReact/src/components/PostList.jsx
@@ -7,13 +7,15 @@ import classes from './PostList.module.css';
 
 const PostList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
+      setIsFetching(true);
       const response = await fetch('http://localhost:8080/posts');
-
       const responseData = await response.json();
-
       setPosts(responseData.posts);
+      setIsFetching(false);
     };
 
     fetchData();
@@ -41,11 +43,18 @@ const PostList = ({ isPosting, onStopPosting }) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && <ul className={classes.posts}>{postContents}</ul>}
-      {posts.length === 0 && (
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>{postContents}</ul>
+      )}
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loding posts...</p>
         </div>
       )}
     </>
